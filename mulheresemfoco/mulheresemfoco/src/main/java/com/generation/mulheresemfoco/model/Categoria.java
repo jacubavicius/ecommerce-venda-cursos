30992873616d4ad340e0f4eb41d47a8107cc7901
Gabriella Jacubavicius <gabriella_jacubavicius@hotmail.com>
2024-07-05 14:11:41 -0300

    feat: desenvolvimento task-3 - Model Categoria

---
+++ mulheresemfoco/mulheresemfoco/src/main/java/com/generation/mulheresemfoco/model/Categoria.java
@@ -0,0 +1,48 @@
+package com.generation.mulheresemfoco.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+
+@Entity
+@Table(name = "tb_categorias")
+public class Categoria {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@NotBlank(message = "O atributo 'categoria' é obrigatório!")
+	private String categoria;
+	
+	@NotBlank(message = "O atributo 'tipo' é obrigatório!")
+	private String tipo;
+	
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getCategoria() {
+		return categoria;
+	}
+
+	public void setCategoria(String categoria) {
+		this.categoria = categoria;
+	}
+
+	public String getTipo() {
+		return tipo;
+	}
+
+	public void setTipo(String tipo) {
+		this.tipo = tipo;
+	}
+
+}
